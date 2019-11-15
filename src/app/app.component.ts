6157df4b82faaed4f3c36cd2b677b112c708119f
Ubuntu <ubuntu@ip-172-31-33-177.ec2.internal>
2019-11-15 12:26:49 +0000

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { NotesService } from 'src/app/notes.service';
+import { Note } from 'src/app/note';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   errMessage: string;
+  notes: Array<Note> = [];
+  note: Note = new Note();
+  constructor(private noteService: NotesService) { }
+  ngOnInit(): void {
+    this.noteService.getNotes().subscribe(res => {
+      this.notes = res;
+    },
+      err => this.errMessage = err.message);
+  }
 
+  save() {
+    if (this.note.title !== undefined && this.note.text !== '') {
+      this.noteService.addNote(this.note).subscribe(res => {
+        this.notes.push(res);
+      }, err => this.errMessage = err.message);
+    } else {
+      this.errMessage = 'Title and Text both are required fields';
+    }
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,31 @@
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from 'src/app/app.component';
+import { HeaderComponent } from 'src/app/header/header.component';
+import { NotesService } from 'src/app/notes.service';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ ],
-  imports: [ ],
-  providers: [ ],
-  bootstrap: [ ]
+  declarations: [AppComponent, HeaderComponent],
+  imports: [BrowserModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    HttpClientModule,
+    FormsModule
+  
+  ],
+  providers: [NotesService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
